b252d53701debbb20d12b4c4d985b79cc8358524
Barani G <73394271+barani-g@users.noreply.github.com>
2026-08-25 11:24:37 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if(s.length !== t.length){
+            return false;
+        }
+        const map = new Map<string, number>();
+        for(const char of s){
+            map.set(char, (map.get(char)||0)+1);
+        }
+        for(const char of t){
+            let count = map.get(char);
+            if(!count){
+                return false;
+            }
+            map.set(char, count-1);
+        }
+        return true;
+    }
+}
